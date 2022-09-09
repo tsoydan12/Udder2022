00562f8b18ba6a0518993ce30d95a5ca1b39bc41
Turgut Soydan <turgutsoydan@gmail.com>
2022-09-09 23:49:17 +0300

    add view vill disappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -16,6 +16,9 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         //
     }
+    override func viewWillDisappear(_ animated: Bool) {
+        //
+    }
 
 
 }
